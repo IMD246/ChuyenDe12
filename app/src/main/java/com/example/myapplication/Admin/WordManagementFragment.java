0a931666ae4d6e3ab1ce4f77081eaacdb90471db
IMD246 <tduy24699@gmail.com>
2021-10-15 02:11:38 +0700

    update fragment

--- app/src/main/java/com/example/myapplication/Admin/WordManagementFragment.java
+++ app/src/main/java/com/example/myapplication/Admin/WordManagementFragment.java
@@ -12,21 +12,8 @@ import android.view.ViewGroup;
 
 import com.example.myapplication.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WordManagementFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WordManagementFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public WordManagementFragment() {
         // Required empty public constructor
@@ -39,33 +26,6 @@ public class WordManagementFragment extends Fragment {
     public void onResume() {
         super.onResume();
     }
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WordManagementFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WordManagementFragment newInstance(String param1, String param2) {
-        WordManagementFragment fragment = new WordManagementFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
