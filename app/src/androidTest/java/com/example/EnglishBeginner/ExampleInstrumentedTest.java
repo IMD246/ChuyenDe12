36601d29f1fdb60b7a993b9640a7c51a94d2b0ae
IMD246 <tduy24699@gmail.com>
2021-11-02 02:49:39 +0700

    update icon app

--- app/src/androidTest/java/com/example/EnglishBeginner/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/EnglishBeginner/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.testapp2;
+package com.example.EnglishBeginner;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.testapp2", appContext.getPackageName());
+        assertEquals("com.example.EnglishBeginner", appContext.getPackageName());
     }
 }
