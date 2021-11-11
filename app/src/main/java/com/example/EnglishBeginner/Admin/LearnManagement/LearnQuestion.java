51b0601804a930240e3ecaf604021fae8daec341
IMD246 <tduy24699@gmail.com>
2021-11-12 02:46:14 +0700

    update learn question

--- app/src/main/java/com/example/EnglishBeginner/Admin/LearnManagement/LearnQuestion.java
+++ app/src/main/java/com/example/EnglishBeginner/Admin/LearnManagement/LearnQuestion.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
