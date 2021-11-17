a8651d9ce930300771c393dbb9214f4a1da68240
trungnghia123456 <68668123+trungnghia123456@users.noreply.github.com>
2021-11-17 15:35:14 +0700

    them mot kieu hoc moi + Splashscreen

--- app/src/main/java/com/example/EnglishBeginner/learn/testing/TestSelectionEnglishFragment.java
+++ app/src/main/java/com/example/EnglishBeginner/learn/testing/TestSelectionEnglishFragment.java
@@ -1,11 +1,13 @@
 package com.example.EnglishBeginner.learn.testing;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +15,9 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.EnglishBeginner.DAO.DAOAnswer;
+import com.example.EnglishBeginner.DTO.Answer;
+import com.example.EnglishBeginner.DTO.Question;
 import com.example.EnglishBeginner.R;
 import com.example.EnglishBeginner.learn.testing.source.UserChooseEnglishAdapter;
 import com.example.EnglishBeginner.learn.testing.source.UserHadChooseEnglishAdapter;
@@ -30,13 +35,29 @@ public class TestSelectionEnglishFragment extends Fragment {
     private ArrayList<String> listUserHadChoose;
     UserChooseEnglishAdapter userChooseEnglishAdapter;
     UserHadChooseEnglishAdapter UserHadChooseEnglishAdapter;
-
+    private Question question;
+    private DAOAnswer daoAnswer;
+    private TestEnglishActivity testEnglishActivity;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.layout_fragment_test_selection_english, container, false);
         setControl();
         setEvent();
+
+        Bundle bundle = getArguments();
+
+        setControl();
+        if (bundle!=null)
+        {
+            question = (Question) bundle.get("question");
+            getDataAnswer(question.getId());
+            tvQuestion.setText(question.getTitle());
+
+        }
+        getDataAnswer(question.getId());
+        Log.d("ubnax", "onCreateView: "+question.getId());
+
         return myView;
     }
 
@@ -54,9 +75,16 @@ public class TestSelectionEnglishFragment extends Fragment {
 
         listUserHadChoose = new ArrayList<>();
         listForUserToChoose = new ArrayList<>();
-        String temp = "i do you not khow are ?";
-        changeStringToArrayList(temp,listForUserToChoose);
+        String answerString = "";
+        if(daoAnswer.getAnswerList().size()>0){
+            for (Answer temp:daoAnswer.getAnswerList()) {
+                answerString += temp.getAnswerQuestion()+" ";
+            }
+            changeStringToArrayList(answerString,listForUserToChoose);
+        }
+        Log.d("asd1p2e", "setAdapter: "+daoAnswer.getAnswerList());
 
+        //
         userChooseEnglishAdapter = new UserChooseEnglishAdapter(getContext(), listForUserToChoose, listUserHadChoose);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 3);// Tạo layout manager
         layoutManager.canScrollHorizontally();
@@ -70,6 +98,7 @@ public class TestSelectionEnglishFragment extends Fragment {
                 NotifyData();
             }
         });
+
         UserHadChooseEnglishAdapter = new UserHadChooseEnglishAdapter(getContext(), listUserHadChoose, listForUserToChoose);
         RecyclerView.LayoutManager layoutManager2 = new GridLayoutManager(getContext(), 3);// Tạo layout manager
         layoutManager2.canScrollHorizontally();
@@ -85,7 +114,9 @@ public class TestSelectionEnglishFragment extends Fragment {
     }
     //Ánh xạ, khởi tạo,...
     private void setControl() {
+        testEnglishActivity =(TestEnglishActivity) getActivity();
         //ánh xạ các view
+        daoAnswer = new DAOAnswer(getContext());
         imgSpeak = myView.findViewById(R.id.img_listen);
         tvQuestion = myView.findViewById(R.id.tv_question);
         rv_listForUserToChoose = myView.findViewById(R.id.recycle_view_button_answer);
@@ -98,5 +129,9 @@ public class TestSelectionEnglishFragment extends Fragment {
         for (String item: temp) {
             listResult.add(item);
         }
+    }
+    private void getDataAnswer(int idQuestion)
+    {
+        daoAnswer.getDataFromFirebase(idQuestion,null);
     }
 }
